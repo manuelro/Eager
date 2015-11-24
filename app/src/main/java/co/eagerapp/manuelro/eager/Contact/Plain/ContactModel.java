ef5b9fd9341938bfe4ec1a2c720517d63a5aa440
Pablo Leon <pleon@dewcom.com>
2015-11-24 10:05:09 -0600

    Fix: ClassModel corrections for List and other Classes edits

--- app/src/main/java/co/eagerapp/manuelro/eager/Contact/Plain/ContactModel.java
+++ app/src/main/java/co/eagerapp/manuelro/eager/Contact/Plain/ContactModel.java
@@ -4,4 +4,24 @@ package co.eagerapp.manuelro.eager.Contact.Plain;
  * Created by Manuel on 19/11/2015.
  */
 public class ContactModel {
+
+    private String email;
+    private int phoneNumber;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
