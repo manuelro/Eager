86c49735e78ac0f23d1f3bc2ce4f36c7b3774403
Felix <juanfe190@hotmail.com>
2015-11-24 19:32:39 -0600

    lista generica funcionando

--- app/src/main/java/co/eagerapp/manuelro/eager/Event/Plain/EventModel.java
+++ app/src/main/java/co/eagerapp/manuelro/eager/Event/Plain/EventModel.java
@@ -1,7 +1,85 @@
 package co.eagerapp.manuelro.eager.Event.Plain;
 
+import java.util.Date;
+
+import co.eagerapp.manuelro.eager.Alarm.Plain.AlarmModel;
+import co.eagerapp.manuelro.eager.EventType.Plain.EventTypeModel;
+
 /**
  * Created by Jose on 24/11/2015.
  */
 public class EventModel {
+    int id;
+    String name;
+    String description;
+    EventTypeModel type;
+    Date startdate;
+    Date finaldate;
+    AlarmModel alarm;
+
+    public EventModel(int id, String name, String description, EventTypeModel type, Date startdate, Date finaldate, AlarmModel alarm) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.startdate = startdate;
+        this.finaldate = finaldate;
+        this.alarm = alarm;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public EventTypeModel getType() {
+        return type;
+    }
+
+    public void setType(EventTypeModel type) {
+        this.type = type;
+    }
+
+    public Date getStartdate() {
+        return startdate;
+    }
+
+    public void setStartdate(Date startdate) {
+        this.startdate = startdate;
+    }
+
+    public Date getFinaldate() {
+        return finaldate;
+    }
+
+    public void setFinaldate(Date finaldate) {
+        this.finaldate = finaldate;
+    }
+
+    public AlarmModel getAlarm() {
+        return alarm;
+    }
+
+    public void setAlarm(AlarmModel alarm) {
+        this.alarm = alarm;
+    }
 }
--- app/src/main/java/co/eagerapp/manuelro/eager/Structures/Lista/Lista.java
+++ app/src/main/java/co/eagerapp/manuelro/eager/Structures/Lista/Lista.java
@@ -1,7 +1,89 @@
 package co.eagerapp.manuelro.eager.Structures.Lista;
 
 /**
- * Created by Pablo on 19/11/2015.
+ * Created by Manuel on 19/11/2015.
  */
 public class Lista {
+
+    Nodo cabeza;
+    Nodo ultimo;
+    int CantidadNodos;
+    public void inserta(Nodo nodo){
+        Nodo aux=cabeza;
+        if(cabeza==null){ //si la lista esta vacia
+            cabeza=nodo; //guardo el nodo
+            ultimo=cabeza;
+            ultimo.setNext(cabeza);
+        }else if(aux!=null){
+            ultimo.setNext(nodo);//creo el nodo como siguiente del ultimo
+            ultimo=ultimo.getNext();//actualizo ultimo
+            ultimo.setNext(cabeza);//enlazo ultimo y cabeza
+        }
+    }
+    public int CuentaNodos() {
+        CantidadNodos = 1;
+        try{
+
+            Nodo aux=cabeza;
+            aux=aux.getNext();
+            CuentaNodosRec(aux);
+
+        }catch(NullPointerException ex){
+            return 0;
+        };
+
+        return CantidadNodos;
+    }
+
+    private void CuentaNodosRec(Nodo N) {
+        if (N != cabeza) {
+            CantidadNodos++;
+            CuentaNodosRec(N.getNext());
+        }
+    }
+
+    @Override
+    public String toString(){
+        Nodo aux = cabeza;
+        String s = "Lista: \n";
+        if(aux != null){
+            s += aux+"";//agrego la cabeza
+            aux = aux.getNext();//avanzo
+            while(aux != cabeza){//cuando sea cabeza otra vez
+                s += aux+"";//se detiene
+                aux = aux.getNext();
+            }
+        }else{
+            s +="vacia";
+        }
+        return s;
+    }
+
+    public void EliminaN(int n){
+        Nodo aux=cabeza;
+        try{
+            if(n==1){
+                ultimo.setNext(aux.getNext());
+                aux.getNext().setBack(ultimo);
+                cabeza=aux.getNext();
+                System.out.println("Eliminado: "+n);
+            }else if(n!=1){
+                int x=2;
+                while(cabeza!=aux.getNext()){
+                    if(n==x){
+                        aux.setNext(aux.getNext().getNext());
+                        System.out.println("Eliminado: "+n);
+                    }
+                    aux=aux.getNext();
+                    x++;
+                }
+            }
+            if(n>CuentaNodos()){
+                System.out.println("Eliminado: null, valor "+n+" no se encuentra");
+            }
+        }catch(NullPointerException e){
+            System.out.println("Vacia");
+        }
+    }
+
 }
