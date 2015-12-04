b4fe766769e650c8a744ef9ea15a6efb0e7ec33b
Pablo Leon <pleon@dewcom.com>
2015-12-04 11:29:33 -0600

    Added Arbol class and method to validate cycle periods v2

---
+++ app/src/main/java/co/eagerapp/manuelro/eager/Structures/Arbol/Nodo.java
@@ -0,0 +1,52 @@
+package co.eagerapp.manuelro.eager.Structures.Arbol;
+
+/**
+ * Created by pablo on 12/3/15.
+ */
+public class Nodo {
+
+    private Object data;
+    private int id;
+    private Nodo hijoDer;
+    private Nodo hijoIzq;
+
+    public Nodo(Object data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString(){
+        return ""+getData();
+    }
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Nodo getHijoDer() {
+        return hijoDer;
+    }
+
+    public void setHijoDer(Nodo hijoDer) {
+        this.hijoDer = hijoDer;
+    }
+
+    public Nodo getHijoIzq() {
+        return hijoIzq;
+    }
+
+    public void setHijoIzq(Nodo hijoIzq) {
+        this.hijoIzq = hijoIzq;
+    }
+}
